dfdc91992e828b63e78a1bb16c1a60570e9d6838
Bake Chris <chris08bake@gmail.com>
2026-03-07 08:01:38 +0000

    Created Studysession Model

--- logs/models.py
+++ logs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class StudySession(models.Model):
+    date = models.DateField()
+    topic = models.CharField(max_length=200)
+    duration = models.IntegerField(help_text ="Duration in minutes")
+    notes = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.topic} - {self.date}"
