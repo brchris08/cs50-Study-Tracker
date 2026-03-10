41051e069e71bec2a0ffa096bb3177c24da2c5f0
Bake Chris <chris08bake@gmail.com>
2026-03-10 11:57:45 +0100

    chore: prepare for deployment to PythonAnywhere

--- study_tracker/settings.py
+++ study_tracker/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = "django-insecure-md+17q!ups_qj#)kw-@=8&dzn)^^fj89)+0!!a^*@bn)c%tp$&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'brchris8.pythonanywhere.com','127.0.0.1'
+]
 
 
 # Application definition
