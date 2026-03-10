914a27b55ad002581d01296e1e20fed3bd15bb9b
Bake Chris <chris08bake@gmail.com>
2026-03-10 09:17:35 +0100

    Updated the Study session model and added two more fields

--- logs/models.py
+++ logs/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 
-# Create your models here.
-class StudySession(models.Model):
-    date = models.DateField()
+class StudySession(models.Model):  # If you named this StudySession, change it to Session
     topic = models.CharField(max_length=200)
-    duration = models.IntegerField(help_text ="Duration in minutes")
+    week_number = models.IntegerField(default=0)
+    date = models.DateField()
+    duration_minutes = models.PositiveIntegerField(default=0)
     notes = models.TextField(blank=True)
 
-    created_at = models.DateTimeField(auto_now_add=True)
-
     def __str__(self):
-        return f"{self.topic} - {self.date}"
+        return f"{self.topic} (Week {self.week_number})"
